dabe5d9936755bfb91bbc5464cd21ffa5a8e9d73
kobayashi-kenta <12321klj@gmail.com>
2020-12-19 00:34:11 +0900

    Initial Commit

---
+++ Swift6TableViewApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6TableViewApp1
+//
+//  Created by Kenta on 2020/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
